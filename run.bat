fdcf18505399b9ff67c2c6ab9d50efa09dfbb0bb
vogler <ralf.vogler@gmail.com>
2012-03-15 00:22:13 +0100

    ignore *mat
    run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,18 @@
 @echo off
-echo generating images
+
+echo 1. generate random images
 call run-data.bat
-echo running octave
+
+echo 2. train neural network in octave
 call run-octave.bat
+
+rem run-PyBrain.bat
+
+echo 3. capture Sudoku using webcam
+cv\cv.exe
+
+echo 4. split image into fields and postprocess
+python split-image.py
+
+echo 5. predict class for each field using trained neural network in octave and call solver
+python processAndPredict.py
