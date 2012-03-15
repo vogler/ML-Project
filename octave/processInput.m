e724979081aae3b7d88f120f883f44b84d2a116a
vogler <ralf.vogler@gmail.com>
2012-03-15 02:05:09 +0100

    predict classes of processed fields

--- octave/processInput.m
+++ octave/processInput.m
@@ -1,20 +1,20 @@
 load('config.mat');
 % set path
 dataPath = sprintf("./%s/", input_folder);
-path = regexprep(dataPath, '\\', '/');
+path = regexprep(dataPath, '/', '\\');
 
 X = []; % matrix to store the image vectors
 field = []; % vector that stores the corresponding sudoku field of a row in X matrix
 
-files = readdir(path);
+files = glob(sprintf('%s%s', path, '*.png'));
 for x=1:length(files)
 	if(!isdir(files{x}))
-		currentFile = sprintf("%s%s", path, files{x});
-		rgbImg = imread(currentFile);
+		rgbImg = imread(files{x});
 		grayImg = rgb2gray(rgbImg);
 		grayImgVector = double(grayImg(:)');
 		X(size(X,1)+1,:) = grayImgVector/255;
-		field(size(field,1)+1,:) = str2num(strsplit(files{x},"."){1});
+        [dir, name, ext, ver] = fileparts(files{x});
+		field(size(field,1)+1,:) = str2num(name);
 	endif
 end
 [pNN, pSVM, pLR] = predict(X);
