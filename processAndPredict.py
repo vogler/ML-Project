0713932b721f8d7a81bf0f49a71b16c43f9e26ac
vogler <ralf.vogler@gmail.com>
2012-03-15 15:38:28 +0100

    refactored: different prediction methods with accuracy-checks
    params

--- processAndPredict.py
+++ processAndPredict.py
@@ -3,6 +3,16 @@ import os, sys
 from nn_predict import predict
 from scipy.io import loadmat
 
+def section(s):
+    print 
+    print 
+    print '-'*6+' '+s+' '+'-'*6
+
+def unique(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if x not in seen and not seen_add(x)]
+
 print "processing input images with octave and writing file octave/inputAndPredictedValues.mat"
 os.system('run-processInput.bat')
 
@@ -12,40 +22,6 @@ input = loadmat('octave/inputAndPredictedValues.mat')
 # input matrix of detected sudoku numbers
 X = input['X']
 
-# predicted values from octave
-field = input['field']
-pNN = input['pNN']
-pSVM = input['pSVM']
-pLR = input['pLR']
-pPyBrain = predict()
-p = pSVM
-
-def section(s):
-    print 
-    print '-'*6+' '+s+' '+'-'*6
-
-def unique(seq):
-    seen = set()
-    seen_add = seen.add
-    return [ x for x in seq if x not in seen and not seen_add(x)]
-
-field_param = ''
-rows = [[] for i in range(9)]
-cols = [[] for i in range(9)]
-predicted_values = [0 for i in range(9*9)]
-for i,c in zip(field, p):
-    i, c = int(i), int(c)
-    predicted_values[i-1] = c
-    row = int((i-1)/9)
-    col = int((i-1)%9)
-    print i, row, col, c
-    field_param += ' '+str(row)+str(col)+str(c)
-    rows[row].append(c)
-    cols[col].append(c)
-
-# accuracy
-section('Accuracy')
-# filter(lambda a: a 0= 0, predicted_values)
 solution = [
 3,0,0, 2,4,0, 0,6,0,
 0,4,0, 0,0,0, 0,5,3,
@@ -56,12 +32,54 @@ solution = [
 0,0,1, 9,2,0, 5,0,0,
 2,0,0, 3,0,0, 7,4,0,
 9,6,0, 5,0,0, 3,0,2]
-correct = [a for (a,b) in zip(solution,predicted_values) if a==b]
-failed  = [(i+1,a,b) for (i,a,b) in zip(range(9*9), solution,predicted_values) if a!=b]
-print 'Correct: ', len(correct), 'Failed: ', len(failed)
-print failed
-print 'Accuracy: ', len(correct)/(9.*9)
+def checkAccuracy(pred, printFailed=False):
+    predicted_values = [0 for i in range(9*9)]
+    for i,c in zip(field, pred):
+        predicted_values[i-1] = c
+    correct         = [a for (a,b) in zip(solution,predicted_values) if a==b]
+    correctNumbers  = len([x for x in correct if x!=0])
+    failed          = [(i+1,a,b) for (i,a,b) in zip(range(9*9), solution,predicted_values) if a!=b]
+    print 'Correct: ', len(correct), 'Failed numbers: ', 42-correctNumbers
+    if printFailed: print failed
+    print 'Accuracy: ', len(correct)/(9.*9)
+    print 'Accuracy for numbers: ', correctNumbers, '/', 42, '->', correctNumbers/42.
 
+def prediction(name, value=[]):
+    pred = value if len(value) else [int(x[0]) for x in input[name]]
+    if -1 in pred: pred = []
+    print name, pred
+    if len(pred): checkAccuracy(pred)
+    print
+    return pred
+
+# predicted values from octave
+field = [int(x[0]) for x in input['field']]
+section('Predictions')
+pNN      = prediction('pNN')
+pSVM     = prediction('pSVM')
+pLR      = prediction('pLR')
+pPyBrain = prediction('pPyBrain', predict())
+
+predictMethod = 'pPyBrain'
+print '-> Using ', predictMethod
+p = eval(predictMethod)
+
+# creating list of params for solver
+field_param = ''
+rows = [[] for i in range(9)]
+cols = [[] for i in range(9)]
+
+for i,c in zip(field, p):
+    row = int((i-1)/9)
+    col = int((i-1)%9)
+    # print i, row, col, c
+    field_param += ' '+str(row)+str(col)+str(c)
+    rows[row].append(c) # used for error checking
+    cols[col].append(c)
+
+# accuracy
+section('Accuracy')
+checkAccuracy(p, True)
 
 section('Valid Sudoku?')
 error = False
