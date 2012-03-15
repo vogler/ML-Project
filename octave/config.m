81cc77922c709b42ac264a6a574428b05d714e44
vogler <ralf.vogler@gmail.com>
2012-03-15 20:15:11 +0100

    models: data3: data and data2 combined

--- octave/config.m
+++ octave/config.m
@@ -5,7 +5,7 @@
 
 function config (lambda=[1, 50, 20], maxIter=100, gamma=1)
 	% Setup parameters
-	data_folder = '../data';  % folder that contains the generated images
+	data_folder = '../data2';  % folder that contains the generated images
     cache_file  = 'cache.mat'; % name of the cache file
 	models_file = 'models.mat'; % contains trained models/parameters of nn/svm/lr
 	input_folder = '../input'; % folder that contains all images from the observed sudoku
